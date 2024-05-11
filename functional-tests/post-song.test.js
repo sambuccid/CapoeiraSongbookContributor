80fc3b77221cf9baef8727fa1938bc7eb835e917
sambuccid <sambuccid@gmail.com>
2024-05-11 12:00:45 +0000

    add first functional test and linting package

---
+++ functional-tests/post-song.test.js
@@ -0,0 +1,15 @@
+const API_URL = "https://nizl5459mi.execute-api.eu-west-2.amazonaws.com";
+const ENDPOINT = "song";
+describe("POST /song", () => {
+  const endpoint_url = `${API_URL}/${ENDPOINT}`;
+
+  it("test runs", async () => {
+    const response = await fetch(endpoint_url, { method: "POST" });
+
+    const respData = await response.text();
+    const respStatus = response.status;
+
+    expect(respStatus).toBe(200);
+    expect(respData).toBe("Hello World! Test");
+  });
+});
