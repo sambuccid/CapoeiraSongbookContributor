dd47be1a9dba4474fbefcb0b43de8dd28bb013b0
sambuccid <sambuccid@gmail.com>
2024-05-18 16:54:52 +0000

    Add script to deploy single lambda

--- deploy.sh
+++ deploy.sh
@@ -1,50 +1,25 @@
 #!/bin/bash
 set -e
 
-install_npm_dependencies () {
-  mv node_modules node_modules_bak
-  mv ./lambdas/node_modules node_modules
-  npm install --omit=dev
-  mv node_modules ./lambdas/node_modules
-  mv node_modules_bak node_modules
-}
-
-echo "Installing non-dev dependencies to be deployed"
-install_npm_dependencies
-
 echo "Deploying S3 bucket for storing lambdas during deployment"
-
 aws cloudformation deploy \
   --stack-name capoeira-songbook-contributor-deployment-bucket \
   --template-file lambda-deployment.yml
   # --capabilities CAPABILITY_IAM
 
-DEPLOYMENT_FILE_NAME=lambdas-$(date +%Y-%m-%d-%H:%M:%S).zip
-
-echo "Packaging lambdas in $DEPLOYMENT_FILE_NAME"
-
-(cd lambdas && zip -r "../$DEPLOYMENT_FILE_NAME" *)
-
-DEPLOYMENT_BUCKET=$(
-  aws cloudformation describe-stacks \
-    --stack-name capoeira-songbook-contributor-deployment-bucket \
-    --query "Stacks[0].Outputs[?OutputKey=='DeploymentBucket'].OutputValue" \
-    --output text
-)
-
-aws s3 cp "./$DEPLOYMENT_FILE_NAME" "s3://$DEPLOYMENT_BUCKET/"
-
-rm "./$DEPLOYMENT_FILE_NAME"
+echo "Packaging lambdas and uploading to S3"
+PACKAGE_LAMBDA_OUTPUT=($(./package-and-push-lambdas.sh))
+LAMBDA_PACKAGE_FILE_NAME=${PACKAGE_LAMBDA_OUTPUT[-1]}
 
 GITHUB_USERNAME=$(<credentials/github_username.txt)
 GITHUB_PASSWORD=$(<credentials/github_personal_access_token.txt)
 
+echo "Updating cloudformation stack"
 aws cloudformation update-stack \
   --stack-name capoeira-songbook-contributor \
-  --parameters "ParameterKey=LambdaS3Key,ParameterValue=$DEPLOYMENT_FILE_NAME" "ParameterKey=GithubUsername,ParameterValue=$GITHUB_USERNAME" "ParameterKey=GithubPassword,ParameterValue=$GITHUB_PASSWORD" \
+  --parameters "ParameterKey=LambdaS3Key,ParameterValue=$LAMBDA_PACKAGE_FILE_NAME" "ParameterKey=GithubUsername,ParameterValue=$GITHUB_USERNAME" "ParameterKey=GithubPassword,ParameterValue=$GITHUB_PASSWORD" \
   --template-body file://cloudformation.yml \
   --capabilities CAPABILITY_NAMED_IAM
-
 
 # aws cloudformation deploy \
 #   --stack-name capoeira-songbook-contributor \
---
+++ package-and-push-lambdas.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+set -e
+
+PACKAGE_FILE_NAME=lambdas-$(date +%Y-%m-%d-%H:%M:%S).zip
+
+install_npm_dependencies () {
+  mv node_modules node_modules_bak
+  mv ./lambdas/node_modules node_modules
+  npm install --omit=dev
+  mv node_modules ./lambdas/node_modules
+  mv node_modules_bak node_modules
+}
+
+package_in_zip_file() {
+  (cd lambdas && zip -r "../$PACKAGE_FILE_NAME" *)
+}
+
+get_deployment_bucket() {
+  local bucket=$(
+    aws cloudformation describe-stacks \
+      --stack-name capoeira-songbook-contributor-deployment-bucket \
+      --query "Stacks[0].Outputs[?OutputKey=='DeploymentBucket'].OutputValue" \
+      --output text
+  )
+  echo "$bucket"
+}
+
+
+echo "Installing non-dev dependencies to be deployed"
+install_npm_dependencies
+
+echo "Packaging lambdas in $PACKAGE_FILE_NAME"
+package_in_zip_file
+
+DEPLOYMENT_BUCKET=$(get_deployment_bucket)
+
+echo "Uploading to S3"
+aws s3 cp "./$PACKAGE_FILE_NAME" "s3://$DEPLOYMENT_BUCKET/"
+
+echo "Removing local lambda package"
+rm "./$PACKAGE_FILE_NAME"
+
+echo "$DEPLOYMENT_BUCKET" # Similar to return value
+echo "$PACKAGE_FILE_NAME" # Similar to return value
