69675ff0d83b5835dccca7f87a1abf20749dc966
sambuccid <sambuccid@gmail.com>
2024-05-19 20:18:37 +0000

    retrofit component tests for git

--- component-tests/lambdas/new-song.test.js
+++ component-tests/lambdas/new-song.test.js
@@ -16,13 +16,13 @@ describe("new-song", () => {
     writeFile: jest.fn(),
     readdir: jest.fn(),
   };
-  const dependencies = {
+  const testDependencies = {
     octokit: octokitSpy,
     spawnSync: spawnSyncSpy,
     fs: fsSpy,
     decryptText, // Passing actual module
   };
-  const envVars = {};
+  const testEnvVars = {};
 
   beforeEach(() => {
     const privateDescriptSpy = jest
@@ -32,18 +32,118 @@ describe("new-song", () => {
   });
 
   describe("usage of git", () => {
+    const credentialFileName = "/tmp/.my-credentials";
+    const spawnSyncInTmpDirectory = {
+      cwd: "/tmp",
+    };
+    const spawnSyncInRepoDirectory = {
+      cwd: "/tmp/CapoeiraSongbook",
+    };
+    const execLambda = async (event, envVars, dependencies) => {
+      if (!event) event = createEvent();
+      if (!envVars) envVars = testEnvVars;
+      if (!dependencies) dependencies = testDependencies;
+      return await lambda(event, envVars, dependencies);
+    };
     it("clones the CapoeiraSongbook repository", async () => {
-      const event = createEvent();
-
-      await lambda(event, envVars, dependencies);
+      await execLambda();
 
       expect(spawnSyncSpy).toHaveBeenCalledWith(
         "git",
         ["clone", "https://github.com/CortinaCapoeira/CapoeiraSongbook.git"],
-        {
-          cwd: "/tmp",
-        }
+        spawnSyncInTmpDirectory
+      );
+    });
+    it("configures git credential file", async () => {
+      await execLambda();
+
+      expect(spawnSyncSpy).toHaveBeenCalledWith(
+        "git",
+        ["config", "credential.helper", `store --file ${credentialFileName}`],
+        spawnSyncInRepoDirectory
+      );
+    });
+    it("creates the credential file with the credentials", async () => {
+      const envVars = {
+        ...testEnvVars,
+        GITHUB_USERNAME: "test-username",
+        GITHUB_PASSWORD: "test-password",
+      };
+
+      await execLambda(undefined, envVars);
+
+      expect(fsSpy.writeFile).toHaveBeenCalledWith(
+        credentialFileName,
+        "https://test-username:test-password@github.com/CortinaCapoeira/CapoeiraSongbook.git"
+      );
+    });
+    it("configures git name and email", async () => {
+      await execLambda();
+
+      expect(spawnSyncSpy).toHaveBeenCalledWith(
+        "git",
+        ["config", "user.email", "songbook-contributor@a.com"],
+        spawnSyncInRepoDirectory
+      );
+
+      expect(spawnSyncSpy).toHaveBeenCalledWith(
+        "git",
+        ["config", "user.name", "Songbook-contributor"],
+        spawnSyncInRepoDirectory
+      );
+    });
+    it("creates new git branch with random name", async () => {
+      await execLambda();
+
+      expect(spawnSyncSpy).toHaveBeenCalledWith(
+        "git",
+        ["checkout", "-b", expect.stringMatching(/^test-\d{1,4}$/)],
+        spawnSyncInRepoDirectory
+      );
+    });
+    it("creates new file", async () => {
+      await execLambda();
+
+      expect(fsSpy.writeFile).toHaveBeenCalledWith(
+        "/tmp/CapoeiraSongbook/abcd-test-file.txt",
+        "a test of writing a file"
+      );
+    });
+    it("adds a new file to git stage", async () => {
+      await execLambda();
+
+      expect(spawnSyncSpy).toHaveBeenCalledWith(
+        "git",
+        ["add", "abcd-test-file.txt"],
+        spawnSyncInRepoDirectory
+      );
+    });
+    it("commits git changes", async () => {
+      await execLambda();
+
+      expect(spawnSyncSpy).toHaveBeenCalledWith(
+        "git",
+        ["commit", "-m", '"Add test file"'],
+        spawnSyncInRepoDirectory
+      );
+    });
+    it("pushes git changes to the branch with a random name", async () => {
+      await execLambda();
+
+      expect(spawnSyncSpy).toHaveBeenCalledWith(
+        "git",
+        [
+          "push",
+          "--set-upstream",
+          "origin",
+          expect.stringMatching(/^test-\d{1,4}$/),
+        ],
+        spawnSyncInRepoDirectory
       );
     });
   });
+
+  // TODO decrypts text correctly (maybe without using return value)
+  // TODO return value + return status code + any other return that we return
+  // TODO all octokit tests
 });
