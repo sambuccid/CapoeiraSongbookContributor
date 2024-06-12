b000744b7eae403bc4bf1be89a28a1fb89cb3435
Sambuccid <sambuccid@gmail.com>
2024-06-12 18:59:03 +0100

    Initial App template

---
+++ app/CapoeiraSongbookContributorApp/app/src/main/java/com/sambccd/capoeirasongbookcontributor/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.sambccd.capoeirasongbookcontributor
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.sambccd.capoeirasongbookcontributor.ui.theme.CapoeiraSongbookContributorTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            CapoeiraSongbookContributorTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                            name = "Grande",
+                            modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+            text = "Hello $name!",
+            modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    CapoeiraSongbookContributorTheme {
+        Greeting("Android")
+    }
+}
